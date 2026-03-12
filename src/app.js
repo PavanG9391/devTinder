cda0c61f3c93939f4ad77381cd40e8cdb518aa89
PavanG9391 <pawangundabathula@gmail.com>
2026-03-12 18:09:24 +0000

    wildcard- error handling in middleware

--- src/app.js
+++ src/app.js
@@ -6,11 +6,19 @@ const {adminAuth} = require('./middlewares/adminAuth')
 app.use('/admin', adminAuth)
 
 app.use('/admin/getAllData',(req,res)=>{
+    throw new err('something wring in your code');
+    
     res.send('get the all the data for the request is successfull');
 })
 
 app.use('/admin/deleteTheUser',(req,res)=>{
     res.send('User data is deleted succesfully');
+})
+
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(500).send('some error has occred in the admin, contact support team.')
+    }
 })
 
 app.listen("3000" , ()=>{
